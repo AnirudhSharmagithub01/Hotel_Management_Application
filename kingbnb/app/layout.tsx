904302193c97f73063f43560218ea3d3c03b5183
AnirudhSharmagithub01 <anirudhsharma857000@gmail.com>
2024-07-19 12:17:22 +0530

    code

---
+++ kingbnb/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "KingHotel",
+  description: "King Hotel is Popular for its Services",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar/>
+        {children}</body>
+    </html>
+  );
+}
---
+++ kingbnb/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div>
+      {/* <h1 className="text-rose-600 text-4xl font-extrabold p-4 ">Hello Supriya</h1> */}
+    </div>
+  );
+}
+ 
